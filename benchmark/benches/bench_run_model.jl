aa333c35b0e8d68487ff732a113e38cf16258574
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2026-09-19 16:00:59 +0200

    Adjust beetle dispersal parameters (#16)

--- benchmark/benches/bench_run_model.jl
+++ benchmark/benches/bench_run_model.jl
@@ -30,7 +30,7 @@ function do_setup_world(n)
                 tick_of_year=20,
                 kernel_scale=1.0,
                 kernel_radius=5,
-                beetles_per_tree=5.0,
+                beetles_per_tree=2.2,
                 trees_per_beetle=1.0,
             ),
         ),
